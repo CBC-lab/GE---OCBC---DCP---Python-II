26bb89cee32257d5d8db28d3ed5e1fe14b222e86
CBC-lab <chuaboonchong93@gmail.com>
2024-07-17 10:09:28 +0800

    Update mainprog.py
    Removed emp4
    Added emp5

--- mainprog.py
+++ mainprog.py
@@ -1,3 +1,6 @@
 from empClass import Employee
-emp4 = Employee("Xavier","Ho","M77777")
-print(emp4.first_name)
+#emp4 = Employee("Xavier","Ho","M77777")
+#print(emp4.first_name)
+emp5 = Employee("Tom","Tan","M88888")
+print(emp5.first_name)
+
